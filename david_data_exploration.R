237d3e361787c23bfbbe0e374c975da3b6137e76
David Reynolds <davidreynolds@mail.missouri.edu>
2020-12-03 22:35:26 -0600

    Data exploration

---
+++ david_data_exploration.R
@@ -0,0 +1,10 @@
+# STAT 8330 project #3
+# David Reynolds
+# Data exploration
+
+# Read in the data
+setwd("~/Downloads/Documents/GitHub/Data-III-Project-3/Data")
+games <- read.csv("games.csv")
+players <- read.csv("players.csv")
+plays <- read.csv("plays.csv")
+track1 <- read.csv("week1.csv")
